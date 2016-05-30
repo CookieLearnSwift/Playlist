4bf058ee1650cbc3260235bf14714dcce61d0ada
cookiegu <cookiegu.se@gmail.com>
2016-05-30 11:01:07 +0900

    Add data model

---
+++ Playlist/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  Playlist
+//
+//  Created by Tingting Gu on 2016/05/30.
+//  Copyright © 2016 Cookrin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+
+}
