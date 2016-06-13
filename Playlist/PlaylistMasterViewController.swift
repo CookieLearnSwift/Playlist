d7a1b6082eb1acc0a3bc7a841d836cfb26362d64
cookiegu <cookiegu.se@gmail.com>
2016-06-13 10:55:55 +0900

    Add playlist and tap action

--- Playlist/PlaylistMasterViewController.swift
+++ Playlist/PlaylistMasterViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
 
+    var playlistArray: [UIImageView] = []
+
     @IBOutlet weak var playlistImageView0: UIImageView!
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        playlistArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView4, playlistImageView5]
+
+        for index in 0..<playlistArray.count {
+            let playlist = Playlist.init(index: index)
+            let playlistImageView = playlistArray[index]
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+        }
 
 //        aButton.setTitle("EnterDetail", forState: .Normal)
 
@@ -32,8 +48,13 @@ class PlaylistMasterViewController: UIViewController {
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showPlaylistDetail" {
-            let playlistDetailViewController : PlaylistDetailViewController? = segue.destinationViewController as! PlaylistDetailViewController
-            playlistDetailViewController!.playlist = Playlist.init(index: 0)
+
+            let playlistImageView = sender!.view! as? UIImageView
+            // find method is unvalid
+            if let index = playlistArray.indexOf(playlistImageView!) {
+                let playlistDetailViewController : PlaylistDetailViewController? = segue.destinationViewController as? PlaylistDetailViewController
+                playlistDetailViewController!.playlist = Playlist.init(index: index)
+            }
         }
     }
 
